11139aafb01ed706da08ddb5bdda6d611ae9d258
karima0072 <250296@astanait.edu.kz>
2025-12-29 05:12:44 +0500

    Create Vehicle.java

---
+++ src/Vehicle.java
@@ -0,0 +1,29 @@
+public abstract class Vehicle {
+    protected String brand;
+    protected int year;
+    protected Driver driver;
+
+    public Vehicle(String brand, int year) {
+        this.brand = brand;
+        this.year = year;
+    }
+
+    public void setDriver(Driver driver) {
+        this.driver = driver;
+    }
+
+    public abstract void startEngine();
+    public abstract void stopEngine();
+
+    public void displayInfo() {
+        System.out.println("Brand: " + brand + ", Year: " + year);
+    }
+
+    public void displayDriver() {
+        if (driver != null) {
+            driver.displayDriverInfo();
+        } else {
+            System.out.println("No driver assigned.");
+        }
+    }
+}
